e024e06ee67be96405bc303fdfe25451cb363847
Rafael Gomes Campos <106031909+RafaelgCampos@users.noreply.github.com>
2023-11-29 10:03:56 -0300

    ADD: Adicionando Página Inicial
    Adicionando base para a Página Inicial. - Ainda não foi finalizada. Adicionada somente parcialmente a parte estética, sem funcionalidades implementadas ainda.

--- aplicativo/app.js
+++ aplicativo/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var homeRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+app.use('/', homeRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
