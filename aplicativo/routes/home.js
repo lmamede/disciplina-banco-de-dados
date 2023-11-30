b1f4823b61877aae252805f9a8015c93582dd52f
Rafael Gomes Campos <rafaelgc@dcc.ufrj.br>
2023-11-30 11:52:06 -0300

    ADD: Modularizando mais o código para facilitar criar novas páginas

--- aplicativo/routes/home.js
+++ aplicativo/routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home'});
+  res.render('home', { title: 'Home', page_title: "NBA Season Stats", page_subtitle: "From 2000 to 2020"});
 });
 
 module.exports = router;
