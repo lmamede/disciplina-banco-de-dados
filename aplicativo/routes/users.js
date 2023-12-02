6f1b23e4899d9ce2069498d124eff3124ad948bd
Rafael Gomes Campos <rafaelgc@dcc.ufrj.br>
2023-12-02 16:54:52 -0300

    ADD: Adicionando páginas restantes
    Adicionando estruturas básicas de funcionamento das páginas, ainda sem conteúdo.

--- aplicativo/routes/users.js
+++ aplicativo/routes/users.js
@@ -10,8 +10,20 @@ router.get('/homeButton', function(req, res, next) {
   res.render('home', { title: 'Home', page_title: "NBA Season Stats", page_subtitle: "From 2000 to 2020"});
 });
 
+router.get('/teamsButton', function(req, res, next) {
+  res.render('teams', { title: 'Teams', page_title: "Teams", page_subtitle: "The Best"})
+});
+
 router.get('/seasonsButton', function(req, res, next) {
   res.render('seasons', { title: 'Seasons', page_title: "Seasons", page_subtitle: "From 2000 to 2020"})
+});
+
+router.get('/arenasButton', function(req, res, next) {
+  res.render('arenas', { title: 'Arenas', page_title: "Arenas", page_subtitle: "Where the Magic Happens"})
+});
+
+router.get('/gamesButton', function(req, res, next) {
+  res.render('games', { title: 'Games', page_title: "Games", page_subtitle: "Check the Matches"})
 });
 
 module.exports = router;
