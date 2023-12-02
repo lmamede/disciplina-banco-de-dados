29c4ee89310eaeb389a51b1a2e3c0f7f9e3d954a
Rafael Gomes Campos <rafaelgc@dcc.ufrj.br>
2023-12-02 16:45:15 -0300

    ADD: Adicionando funcionalidade para os botões de troca de página

--- aplicativo/routes/users.js
+++ aplicativo/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/homeButton', function(req, res, next) {
+  res.render('home', { title: 'Home', page_title: "NBA Season Stats", page_subtitle: "From 2000 to 2020"});
+});
+
+router.get('/seasonsButton', function(req, res, next) {
+  res.render('seasons', { title: 'Seasons', page_title: "Seasons", page_subtitle: "From 2000 to 2020"})
+});
+
 module.exports = router;
