313b64e4b849273ec8c62c4b6752cc16da992e39
Lorena Mamede <lorenamb@dcc.ufrj.br>
2023-12-04 09:58:09 -0300

    Create new frontend project and integrate with backend
    Adding allow cross origin header, so request can be done to the backend.
    Create a useFetch hook to centralize requests config use react native
    implementation. Installing router lib to future pages redirects.

--- aplicativo-backend/src/main/java/com/nbastats/aplicativobackend/controller/NBAStatsController.java
+++ aplicativo-backend/src/main/java/com/nbastats/aplicativobackend/controller/NBAStatsController.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(path = "/nba")
-
 public class NBAStatsController {
     @Autowired
     private NBAStatsService nbaStatsService;
